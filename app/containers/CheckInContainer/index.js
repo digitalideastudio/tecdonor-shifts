7b3eb9eda0dc0542803f56d29ffc17377c8d8e2c
Volodymyr Antoniuk <volodymyr@digitalidea.studio>
2018-02-26 18:45:53 +0200

    Adds keyboard avoid view

--- app/containers/CheckInContainer/index.js
+++ app/containers/CheckInContainer/index.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
-import { View, Text, SafeAreaView, StatusBar } from 'react-native';
+import { View, Text, StatusBar, KeyboardAvoidingView } from 'react-native';
 import PropTypes from 'prop-types';
 import { Container } from '../../components/Container';
 import { Footer } from './components/Footer';
 import { BackgroundImage } from '../../components/BackgroundImage';
 import { InputWithIcon } from '../../components/TextInput';
 import { ThreeAsterisk } from '../../components/Icon';
-import { FontAwesome as Icon } from '@expo/vector-icons';
 
 import styles from './styles';
 
@@ -15,42 +14,46 @@ class CheckInContainer extends Component {
         return (
             <Container>
                 <StatusBar barStyle="light-content"/>
-                <View style={styles.header}>
-                    <BackgroundImage
-                        source={{ uri: 'https://tecdonor-devedge.s3.amazonaws.com/photos/b58ffb817335568fbfbe7d1c4935c22e/ecf9c7b87de0c2ff0090bbea25cc72ca.jpg' }}
-                    />
-                    <View style={styles.titles}>
-                        <View>
-                            <Text style={styles.pageTitleText}>
-                                Children's Hope
-                            </Text>
-                            <Text style={styles.pageSubTitleText}>
-                                Offered by: Hopely
-                            </Text>
+                <KeyboardAvoidingView
+                    behavior={'padding'}
+                    style={{ flex: 1 }}
+                >
+                    <View style={styles.header}>
+                        <BackgroundImage
+                            source={{ uri: 'https://tecdonor-devedge.s3.amazonaws.com/photos/b58ffb817335568fbfbe7d1c4935c22e/ecf9c7b87de0c2ff0090bbea25cc72ca.jpg' }}
+                        />
+                        <View style={styles.titles}>
+                            <View>
+                                <Text style={styles.pageTitleText}>
+                                    Children's Hope
+                                </Text>
+                                <Text style={styles.pageSubTitleText}>
+                                    Offered by: Hopely
+                                </Text>
+                            </View>
                         </View>
                     </View>
-                </View>
-                <View style={styles.main}>
-                    <View style={styles.mainSpacer}/>
-                    <View style={styles.inputContainer}>
-                        <InputWithIcon
-                            icon={<ThreeAsterisk/>}
-                            buttonText={'CONFIRM'}
-                            password
-                            placeholder={'PERSONAL PIN'}
-                            inputStyle={{ textAlign: 'center' }}
-                            keyboardType={'numeric'}
-                        />
+
+                    <View style={styles.main}>
+                        <View style={styles.mainSpacer}/>
+                        <View style={styles.inputContainer}>
+                            <InputWithIcon
+                                icon={<ThreeAsterisk/>}
+                                buttonText={'CONFIRM'}
+                                password
+                                placeholder={'PERSONAL PIN'}
+                                inputStyle={{ textAlign: 'center' }}
+                                keyboardType={'numeric'}
+                            />
+                        </View>
                     </View>
-                </View>
+                </KeyboardAvoidingView>
                 <Footer/>
-            </Container>
+            </Container >
         );
     }
 }
 
-CheckInContainer.propTypes = {
-
-};
+CheckInContainer.propTypes = {};
 
 export default CheckInContainer;
