fc5d8dd95f468f2f0843f547eb594d38981e41b2
Volodymyr Antoniuk <volodymyr@digitalidea.studio>
2018-03-07 19:50:39 +0200

    Improve CommonText component

--- app/components/Common/CommonText.js
+++ app/components/Common/CommonText.js
@@ -3,12 +3,17 @@ import { Text } from 'react-native';
 import PropTypes from 'prop-types';
 import styles from './styles';
 
-const CommonText = ({ children }) => (
-    <Text style={styles.text}>{children}</Text>
+const CommonText = ({ children, style, ...props }) => (
+    <Text
+        style={[styles.text, style]}
+        {...props}
+    >
+        {children}
+    </Text>
 );
 
 CommonText.propTypes = {
-    children: PropTypes.string
+    children: PropTypes.string,
 };
 
 export default CommonText;
