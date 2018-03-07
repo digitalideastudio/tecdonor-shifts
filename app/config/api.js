99dc7fb6b08d33da8eb65cb34fc86661bec01307
Volodymyr Antoniuk <volodymyr@digitalidea.studio>
2018-03-07 19:27:33 +0200

    Add app logo and splash

--- app/config/api.js
+++ app/config/api.js
@@ -2,6 +2,6 @@ export const HOST_NAME = 'https://dev.tecdonor.com';
 export const PREFIX = '/api/mobile';
 export const PRE = `${HOST_NAME}${PREFIX}`;
 export const LOGIN = `${PRE}/login`;
-export const GET_SHIFTS = `${PRE}/nonprofit/get_upcoming_shifts?take=3`;
+export const GET_SHIFTS = `${PRE}/nonprofit/get_upcoming_shifts?take=50`;
 export const GET_SHIFT_STATUS = `${PRE}/volunteer/shift_status`;
 export const CHECK_IN_OUT = `${PRE}/volunteer/check_in_out`;
