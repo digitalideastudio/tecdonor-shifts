9ae28d853a698e05be1764d456f773ab6525aaf2
Volodymyr Antoniuk <volodymyr@digitalidea.studio>
2018-03-08 19:22:57 +0200

    Fix error message parser

--- app/utils/validation.js
+++ app/utils/validation.js
@@ -1,11 +1,18 @@
 export function getValidationErrorString( valResult ) {
+    console.log('error object: ', valResult);
     if (!valResult) return;
 
     let errorString = '';
 
     Object.keys(valResult).forEach((key) => {
-         errorString += `${valResult[key][0]}\n`;
+        if (Array.isArray(valResult[key])) {
+            errorString += `${valResult[key][0]}\n`;
+        } else {
+            errorString += `${valResult[key]}\n`;
+        }
     });
+
+    console.log('parsed message: ', errorString);
 
     return errorString.trim();
 }
